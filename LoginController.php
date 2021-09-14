b09e0c7ade795a22be8ae9592cff5ae525faf454
peekinternational <peekinternational@gmail.com>
2021-09-14 12:35:41 +0500

    3rd Commit with testing

---
+++ LoginController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+class LoginController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+        
+        if(!auth()->attempt($request->only('username', 'password'), $request->remember)){
+            return back()->with('status', 'Invalid login details');
+        }
+
+        return redirect()->route('home')->with('message', 'Welcome Back!');
+    }
+}
