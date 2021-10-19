8e93574ce6f72870c0650a71e73dea52ce423721
peekinternational <peekinternational@gmail.com>
2021-10-19 12:34:06 +0500

    Push Lang update to live

--- resources/views/user/setting/profile.blade.php
+++ resources/views/user/setting/profile.blade.php
@@ -1,20 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <div class="bg-secondary py-4">
-        <div class="container pt-2 pb-3">
-            <div class="d-flex flex-column flex-md-row align-items-center">
-                <a href="/project-listing"
-                    class="btn btn-block bg-gray-800 text-white w-md-auto mt-2 mr-md-2">{{ __('browseProject') }}</a>
-                <a href="/contest-listing"
-                    class="btn btn-block bg-gray-800 text-white w-md-auto mr-md-2">{{ __('browseContest') }}</a>
-                <a href="/browse/category"
-                    class="btn btn-block bg-gray-800 text-white w-md-auto mr-md-2">{{ __('browseCategories') }}</a>
-                <a href="/showcases"
-                    class="btn btn-block bg-gray-800 text-white w-md-auto mr-md-2">{{ __('showcase') }}</a>
-                <a href="/post-contest" class="btn btn-block btn-primary w-md-auto ml-auto">{{ __('startContest') }}</a>
-            </div>
-        </div>
-    </div>
+    <x-head-links></x-head-links>
 
     <section class="container py-5">
         <div class="row">
